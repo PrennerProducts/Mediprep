f91d8ed628b74d1a12dccdb9e876ec6f7924e50a
natariii <rene.angerer@gmail.com>
2022-12-13 22:03:32 +0100

    DateTimePicker+

---
+++ components/DateTimePicker.js
@@ -0,0 +1,107 @@
+import React, {useState} from 'react';
+import {Modal, Text, TouchableHighlight, View, Platform} from 'react-native';
+
+import moment from 'moment';
+import DatePicker from "react-native-date-picker";
+
+const CustomDatePicker = (props) => {
+    const { textStyle } = props;
+    const [datum, setDatum] = useState(moment());
+    const [show, setShow] = useState(false);
+
+    const onChange = (e, selectedDate) => {
+        setDatum(moment(selectedDate));
+    }
+    const onAndroidChange = (e, selectedDate) => {
+        setShow(false)
+        if(selectedDate){
+            setDatum(moment(selectedDate));
+            props.onDateChange(selectedDate);
+        }
+    }
+    const onDonePress = () => {
+        props.onDateChange(datum);
+        setShow(false);
+    }
+
+    const renderDatePicker = () => {
+        return(
+            <DateTimePicker
+                timeZoneOffsetInMinutes={0}
+                value={new Date(datum)}
+                mode="date"
+                minimumDate={new Date(moment().subtract(120, 'years').format('YYYY-MM-DD'))}
+                maximumDate={new Date(moment().format('YYYY-MM-DD'))}
+                onChange={Platform.OS === 'ios' ? onChange : onAndroidChange}
+                />
+
+        );
+    }
+
+    return(
+        <TouchableHighlight
+            activeOpacity={0}
+            onPress={() => setShow(true)}>
+            <View>
+                <Text style={textStyle}>{datum.format('MMMM Do, YYYY')} </Text>
+
+                {Platform.OS === 'android' && show && renderDatePicker()}
+                {Platform.OS === 'ios' && (
+                    <Modal
+                        transparent={true}
+                        animationType="slide"
+                        visible={show}
+                        supportedOrientations={['portrait']}
+                        onRequestClose={() => setShow(false)}>
+                        <View style={{flex:1}}>
+                            <TouchableHighlight
+                                style={{
+                                    flex: 1,
+                                    alignItems: 'flex-end',
+                                    flexDirection: 'row',
+                                }}
+                                activeOpacity={1}
+                                visible={show}
+                                onPress={() => setShow(false)}>
+                                <TouchableHighlight
+                                    underlayColor={'#FFFFFF'}
+                                    style={{
+                                        flex: 1,
+                                        borderTopColor: '#E9E9E9',
+                                        borderTopWidth: 1,
+                                    }}
+                                    onPress={() => console.log('datepicker clicked')}>
+
+                                    <View style={{
+                                        backgroundColor: '#FFFFFF',
+                                        height: 256,
+                                        overflow: 'hidden',
+                                    }}>
+                                        <View style={{margin: 20 }}>
+                                            {renderDatePicker()}
+                                        </View>
+                                        <TouchableHighlight
+                                            underlayColor={'transparent'}
+                                            onPress={onDonePress}
+                                            style={[styles.btnText, styles.btnDone]}>
+                                            <Text>
+                                                Done
+                                            </Text>
+                                        </TouchableHighlight>
+                                    </View>
+
+                                </TouchableHighlight>
+                            </TouchableHighlight>
+                        </View>
+                    </Modal>
+                )}
+            </View>
+        </TouchableHighlight>
+    )
+};
+
+DatePicker.defaultProps = {
+    textStyle: {},
+};
+
+export default CustomDatePicker;
