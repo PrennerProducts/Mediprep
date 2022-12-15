1d64b08c8b7e342ef5e4a81365220fa3b6e9ed6c
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2022-12-15 11:35:11 +0100

    MedikamenteVorbereiten Screen init

---
+++ screens/MedikamenteVorbereiten.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet, Animated } from 'react-native';
+import { useState } from 'react';
+
+export const MedikamenteVorbereiten = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>MedikamenteVorbereiten</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default MedikamenteVorbereiten;
