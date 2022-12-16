bf90723bb49ea19debc0520855b9be3f3d189fb9
Robert <gr7054@mci4me.at>
2022-12-16 18:40:42 +0100

    Push

--- screens/MedikamentenUebersichtScreen.js
+++ screens/MedikamentenUebersichtScreen.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet, Animated } from 'react-native';
+import { View, Text, StyleSheet, Animated, TouchableOpacity } from 'react-native';
 import Tablettenbox from '../components/Tablettenbox';
 import { Schachtel } from '../models/Schachtel';
 import { MedikamentenListe } from '../models/MedikamentenListe';
 import TablettenStueckAnzeige from '../components/TablettenStueckAnzeige';
 import WeiterButton from '../components/WeiterButton';
 
-const MedikamentenUebersichtScreen = (props) => {
+const MedikamentenUebersichtScreen = ({navigation}) => {
+    const pressHandler4 = () => {
+      navigation.navigate('Befullungstarten');
+    };
   return (
     <View style={{ backgroundColor: '#25b5e9' }}>
       <Text style={styles.title}>MedikamentenUebersichtScreen</Text>
+      <TouchableOpacity onPress={pressHandler4}>
       <WeiterButton style={styles.weiterButton} />
+      </TouchableOpacity>
     </View>
   );
 };
