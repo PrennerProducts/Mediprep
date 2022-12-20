2e3374be7eb95911747e7d1b3a64e459e9a579d2
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2022-12-20 11:51:33 +0100

    codereview unnötige Imports entfernt

--- screens/MediprepNavigator.js
+++ screens/MediprepNavigator.js
@@ -1,86 +1,72 @@
-// Test
-
-import { setStatusBarBackgroundColor } from 'expo-status-bar';
-import React from 'react';
-import {View, Text, Alert} from 'react-native';
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator, HeaderStyleInterpolators } from 'react-navigation-stack';
-
+import { createStackNavigator } from 'react-navigation-stack';
 import Homescreen from './Homescreen';
 import Ladebildschirm from './Ladebildschirm';
-
 import Befullungstarten from './MedikamenteVorbereiten';
 import GreatSuccessScreen from './GreatSuccessScreen';
 import MedikamentenUebersichtScreen from './MedikamentenUebersichtScreen';
 import WochenTagAuswahlScreen from './WochentagAuswahlScreen';
 import MedikamentenanzeigeScreen from './MedikamentenanzeigeScreen';
-import Kontrolle from "./Kontrolle";
+import Kontrolle from './Kontrolle';
 
 const screens = {
+  Homescreen: {
+    screen: Homescreen,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: false,
+    }),
+  },
 
-    Homescreen: {
-        screen: Homescreen,
-        navigationOptions: ({ navigation }) => ({
-            headerShown: false,
-          }),
-    },
+  Ladebildschirm: {
+    screen: Ladebildschirm,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: false,
+    }),
+  },
 
-    Ladebildschirm: {
-        screen: Ladebildschirm,
-        navigationOptions: ({ navigation }) => ({
-            headerShown: false,
-          }),
-    },
+  Befullungstarten: {
+    screen: Befullungstarten,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: false,
+    }),
+  },
 
-    Befullungstarten: {
-        screen: Befullungstarten,
-        navigationOptions: ({ navigation }) => ({
-            headerShown: false,
-          }),
-    },
+  GreatSuccessScreen: {
+    screen: GreatSuccessScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: false,
+    }),
+  },
 
-    GreatSuccessScreen: {
-        screen: GreatSuccessScreen,
-        navigationOptions: ({ navigation }) => ({
-            headerShown: false,
-          }),
-    },
+  MedikamentenUebersichtScreen: {
+    screen: MedikamentenUebersichtScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: false,
+    }),
+  },
 
-    MedikamentenUebersichtScreen: {
-        screen: MedikamentenUebersichtScreen,
-        navigationOptions: ({ navigation }) => ({
-            headerShown: false,
-          }),
-    },
+  WochenTagAuswahlScreen: {
+    screen: WochenTagAuswahlScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: false,
+    }),
+  },
 
-    WochenTagAuswahlScreen: {
-        screen: WochenTagAuswahlScreen,
-        navigationOptions: ({ navigation }) => ({
-            headerShown: false,
-          }),
-    },
+  MedikamentenanzeigeScreen: {
+    screen: MedikamentenanzeigeScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: false,
+    }),
+  },
 
-    MedikamentenanzeigeScreen: {
-        screen: MedikamentenanzeigeScreen,
-        navigationOptions: ({ navigation }) => ({
-            headerShown: false,
-          }),
-    },
-
-    Kontrolle: {
-        screen: Kontrolle,
-        navigationOptions: ({ navigation }) => ({
-            headerShown: false,
-        }),
-    },
-    
-}
+  Kontrolle: {
+    screen: Kontrolle,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: false,
+    }),
+  },
+};
 
 const MediprepNavigator = createStackNavigator(screens);
-
-
-
-
-
 
 export default createAppContainer(MediprepNavigator);
