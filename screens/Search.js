a75e6562b78c4278dd152ec2639d7a60a444ba34
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2023-01-11 21:47:31 +0100

    search update auf API

--- screens/Search.js
+++ screens/Search.js
@@ -1,57 +1,11 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
-import { ListItem, SearchBar } from "react-native-elements";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { ListItem, SearchBar } from 'react-native-elements';
 import { MedikamentenListe } from '../models/MedikamentenListe';
+import { getMedikamenteFromApi } from '../data/Api';
 
-
-
-const DATA = [
-    {
-      id: "1",
-      title: MedikamentenListe.MLDummy.getMedikamentName(1),
-    },
-    {
-      id: "2",
-      title: MedikamentenListe.MLDummy.getMedikamentName(2),
-    },
-    {
-      id: "3",
-      title: MedikamentenListe.MLDummy.getMedikamentName(3),
-    },
-    {
-      id: "4",
-      title: MedikamentenListe.MLDummy.getMedikamentName(4),
-    },
-    {
-      id: "5",
-      title: MedikamentenListe.MLDummy.getMedikamentName(5),
-    },
-    {
-      id: "6",
-      title: MedikamentenListe.MLDummy.getMedikamentName(6),
-    },
-    {
-      id: "7",
-      title: MedikamentenListe.MLDummy.getMedikamentName(7),
-    },
-    {
-      id: "8",
-      title: MedikamentenListe.MLDummy.getMedikamentName(8),
-    },
-    {
-      id: "9",
-      title: MedikamentenListe.MLDummy.getMedikamentName(9),
-    },
-    {
-      id: "10",
-      title: MedikamentenListe.MLDummy.getMedikamentName(10),
-    },
-
-  
-  ];
-
-
-
+let medtitle = MedikamentenListe.MLDummy.getMedikamentName(1);
+let maxmeds = MedikamentenListe.MLDummy.getmaxmeds;
 
 const Item = ({ title }) => {
   return (
@@ -60,21 +14,21 @@ const Item = ({ title }) => {
     </View>
   );
 };
-  
+
 const renderItem = ({ item }) => <Item title={item.title} />;
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: false,
-      data: DATA,
       error: null,
-      searchValue: "",
+      searchValue: '',
     };
-    this.arrayholder = DATA;
   }
-  
-  searchFunction = (text) => {
+
+  searchFunction = async (text) => {
+    data = await getMedikamenteFromApi();
+    this.arrayholder = data;
     const updatedData = this.arrayholder.filter((item) => {
       const item_data = `${item.title.toUpperCase()})`;
       const text_data = text.toUpperCase();
@@ -84,7 +38,6 @@ class Search extends Component {
   };
   render() {
     return (
-        
       <View style={styles.container}>
         <SearchBar
           placeholder="Search Here..."
@@ -102,7 +55,6 @@ class Search extends Component {
       </View>
     );
   }
-
 }
 
 export default Search;
@@ -113,11 +65,9 @@ const styles = StyleSheet.create({
     padding: 2,
   },
   item: {
-    backgroundColor: "lightblue",
+    backgroundColor: 'lightblue',
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
   },
-})
-
-
+});
