295c65e980e1681e80a820e392ce3ebbb7132e6e
Chris Z <zc2654@mci4me.at>
2023-01-16 23:24:11 +0100

    TageszeitenScreen Speicherung anderes gelöst

--- screens/TageszeitenScreen.js
+++ screens/TageszeitenScreen.js
@@ -8,73 +8,100 @@ import { SelectList } from 'react-native-dropdown-select-list'
 
 
 export const TageszeitenScreen = ({ navigation }) => {
+    let finalAuswahl = [0, 0, 0, 0];
+    const [selectedValue, setSelectedValue] = useState(null);
+
     //Array bei jedem Neuladen in die Seite auf 0 setzen
     const pressHandlerBack = () => {
-        ScreenObserver.dayly = false;
-        ScreenObserver.days = [];
-        navigation.navigate('Homescreen');
+        ScreenObserver.dosierung = [0, 0, 0, 0];
+        navigation.navigate('TagAuswahlScreen');
     };
     const pressHandler = () => {
-        
-        
+        ScreenObserver.dosierung = [selectedMorgen,selectedMittag,selectedAbends,selectedNacht];
+        console.log('FinalAuswahl' +finalAuswahl);  //Variable finalAuswahl beinhaltet die ausgewählten Zeiten im ArrayStyle [morgens, mittags, abends, nachts]
+        alert('Auswahl:  ' + ScreenObserver.dosierung );
+
+        ScreenObserver.dosierung = finalAuswahl;
+
+        console.log('Dosierung:' +ScreenObserver.dosierung);
+
         navigation.navigate('TageszeitenScreen');
     };
 
-        const [selected, setSelected] = React.useState("");
-        
-        const data = [
-            {key:'1', value:'Nie einnehmen',  s:'1'},
-            {key:'2', value:'1/4 Tablette', s:'1/4'},
-            {key:'3', value:'1/2 Tablette', s:'1/2'},
-            {key:'4', value:'1 Tablette', s:'1'},
-            {key:'5', value:'1 1/2 Tabletten', s:'1 1/2'},
-            {key:'6', value:'2 Tabletten', s:'2'},
-            {key:'7', value:'3 Tabletten', s:'3'},
-            {key:'8', value:'4 Tabletten', s:'4'},
-        ]
+
+
+    const [selectedMorgen, setSelectedMorgen] = useState(null);
+    const [selectedMittag, setSelectedMittag] = useState(null);
+    const [selectedAbends, setSelectedAbends] = useState(null);
+    const [selectedNacht, setSelectedNacht] = useState(null);
+
+    const data = [
+        {key:'0', value:'Nie einnehmen'},
+        {key:'0.25', value:'1/4 Tablette'},
+        {key:'0.5', value:'1/2 Tablette'},
+        {key:'1', value:'1 Tabletten'},
+        {key:'1.5', value:'1 1/2 Tabletten'},
+        {key:'2', value:'2 Tabletten'},
+        {key:'3', value:'3 Tabletten'},
+        {key:'4', value:'4 Tabletten'}
+    ]
 
     return (
         <View style={styles.container}>
             <Text style={styles.textfontINT}>
                 Wie oft möchten Sie die Medikamente einnehmen?
             </Text>
-           <View style={styles.container3}>
-            <Text style={styles.textfontzeit}>Morgens </Text>
-            <SelectList 
-            search={false}
-            defaultOption={data[0]}
-            setSelected={(val) => setSelected(val)} 
-            data={data} 
-            save="s" />
+            <View style={styles.container3}>
+                <Text style={styles.textfontzeit}>Morgens </Text>
+                <SelectList
+                    search={false}
+                    defaultOption={data[0]}
+                    setSelected={(val) => setSelectedMorgen(val)}
+                    //setSelect={(val) => handleSelectmorgens(val)}
+                    data={data}
+                    save="key"
+
+
+                />
             </View>
             <View style={styles.container3}>
-            <Text style={styles.textfontzeit}>Mittags </Text>
-            <SelectList 
-            search={false}
-            defaultOption={data[0]}
-            setSelected={(val) => setSelected(val)} 
-            data={data} 
-            save="value" />
+                <Text style={styles.textfontzeit}>Mittags </Text>
+                <SelectList
+                    search={false}
+                    defaultOption={data[0]}
+                    setSelected={(val) => setSelectedMittag(val)}
+                    //setSelected={(val) => handleSelectmittags(val)}
+                    data={data}
+                    save="key"
+                />
+
             </View>
             <View style={styles.container3}>
-            <Text style={styles.textfontzeit}>Abends </Text>
-            <SelectList 
-            search={false}
-            defaultOption={data[0]}
-            setSelected={(val) => setSelected(val)} 
-            data={data} 
-            save="value" />
+                <Text style={styles.textfontzeit}>Abends </Text>
+
+                <SelectList
+                    search={false}
+                    defaultOption={data[0]}
+                    setSelected={(val) => setSelectedAbends(val)}
+                    //setSelected={(val) => handleSelectabends(val)}
+                    data={data}
+                    save="key"
+
+
+                />
             </View>
             <View style={styles.container3}>
-            <Text style={styles.textfontzeit}>Nachts </Text>
-            <SelectList 
-            search={false}
-            defaultOption={data[0]}
-            setSelected={(val) => setSelected(val)} 
-            data={data} 
-            save="value" />
+                <Text style={styles.textfontzeit}>Nachts </Text>
+                <SelectList
+                    search={false}
+                    defaultOption={data[0]}
+                    setSelected={(val) => setSelectedNacht(val)}
+                    //setSelected={(val) => handleSelectnachts(val)}
+                    data={data}
+                    save="key"
+                />
             </View>
-            
+
             <View style={styles.buttonsContainer}>
                 <TouchableOpacity onPress={pressHandlerBack}>
                     <ZurueckButton style={styles.button} />
@@ -86,7 +113,7 @@ export const TageszeitenScreen = ({ navigation }) => {
         </View>
     );
 
-    
+
 };
 
 
@@ -160,39 +187,9 @@ const styles = StyleSheet.create({
 
 
     },
-    rahmen: {
-        paddingTop: 20,
-        bottom: 30,
-        width: 250,
-    },
-    rahmen1: {
-        flexDirection: 'row',
-        paddingTop: 20,
-        bottom: 30,
-
-    },
-    farblicheauswahl: {
-        marginTop: 15,
-        //flexDirection: 'row',
-        //borderWidth: 3,
-        //borderColor: '#6b93ff',
-        //borderRadius: 30,
-        backgroundColor: '#032E5B',
-        justifyContent: 'center',
-        alignItems: 'center',
 
 
-    },
-    farblicheauswahlActive: {
-        marginTop: 15,
-        //flexDirection: 'row',
-        //borderWidth: 5,
-        //borderColor: '#6b93ff',
-        //borderRadius: 30,
-        backgroundColor: 'lightblue',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+
 
     wochentag: {
         fontSize: 35,
@@ -230,7 +227,7 @@ const styles = StyleSheet.create({
         elevation: 24,
     },
     container3: {
-     
+
         marginTop: 30,
         bottom: 10,
         flexDirection: 'row',
