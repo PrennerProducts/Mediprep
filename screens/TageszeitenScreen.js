d2109f8b613e522db4b81ac4077bd8f3cc359d44
Robert <gr7054@mci4me.at>
2023-01-18 20:07:05 +0100

    vereinheitlicht

--- screens/TageszeitenScreen.js
+++ screens/TageszeitenScreen.js
@@ -176,7 +176,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: 'black',
         //elevation: 42,
-        textAlign: 'left',
+        textAlign: 'center',
     },
 
     tagauswahltext: {
