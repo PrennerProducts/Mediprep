8bc2a1cee7d9b4ed622a5ad9e0a0c0c18c25b4e0
PrennerProducts <61316223+PrennerProducts@users.noreply.github.com>
2023-01-18 18:54:50 +0100

    funktioniert nicht!

--- screens/Search.js
+++ screens/Search.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableHighlight } from 'react-native';
+import React, { Component, useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableHighlight,SafeAreaView, StatusBar,TouchableOpacity, Alert } from 'react-native';
 import { ListItem, SearchBar } from 'react-native-elements';
 import { Medikament } from '../models/Medikament';
 import { getMedikamenteFromApi } from '../data/Api';
@@ -16,6 +16,11 @@ const Item = ({ name }) => {
 };
 
 const renderItem = ({ item }) => <Item name={item.name} />;
+
+
+
+
+// Class beginnt hier//
 class Search extends Component {
   constructor(props) {
     super(props);
@@ -23,15 +28,27 @@ class Search extends Component {
       loading: false,
       error: null,
       searchValue: '',
+
+      //selectedId State
+      selectedId: null,
+      tempItem: '',
+      tempText: '',
+      tempKy: '',
+
     };
   }
+
+ 
 
   searchFunction = async (text) => {
     let data = await getMedikamenteFromApi();
     this.arrayholder = data;
     const updatedData = this.arrayholder.filter((item) => {
       const item_data = `${item.name.toUpperCase()})`;
+      this.tempItem = item_data;
       const text_data = text.toUpperCase();
+      this.tempText = text_data;
+      console.log('TTTTTTTT ITEMDATA: ' + item_data );
       return item_data.indexOf(text_data) > -1;
     });
     this.setState({ data: updatedData, searchValue: text });
@@ -39,9 +56,14 @@ class Search extends Component {
 
   // Mit Klick auf ein Suchergebnis Speichern des Medikamentennames im temporären Objekt und weiter zu Abfrage Intervall.
   onPressHandler = () => {
+    //this.setState({selectedId: this.state.item.name});
+    console.log('This.tempItem: '+ this.state.selectedId); ;
+
+
+    
     const tempMedikament = new Medikament(this.state.data[0].name, this.state.data[0].img);
     ScreenObserver.tempMed = tempMedikament;
-    
+  
     //Weiterleitung zu Screen Abfrage Intervall
     // ... 
 
@@ -54,6 +76,10 @@ class Search extends Component {
     this.props.navigation.navigate('TagAuswahlScreen');
 
     }
+
+   
+
+
   render() {
     return (
       <View style={styles.container}>
@@ -74,7 +100,10 @@ class Search extends Component {
           data={this.state.data}
           renderItem={renderItem}
           keyExtractor={(item) => item.name}
+          onSelect={console.log('Selected: ' + this.state.tempItem)}
+
         />
+     
         </TouchableHighlight>
       </View>
       </View>
